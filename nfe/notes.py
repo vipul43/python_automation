2a7b93ca1b7848d089c8828e71c50d223469d71c
vipul43 <saifunny43@gmail.com>
2020-05-09 22:41:56 +0530

    - added checks for filextension, foldername and filename - checks simply opens up the corresponding element if it exists, else creates that element and opens it up

--- nfe/notes.py
+++ nfe/notes.py
@@ -1,6 +1,24 @@
 import sys
 import os
 
+
+#adding file extensions
+text = ".txt"
+markdown = ".md"
+python = ".py"
+c = ".c"
+cpp = ".cpp"
+matlab = ".m"
+shell = ".sh"
+javascript = ".js"
+typescript = ".ts"
+html = ".html"
+css = ".css"
+
+java = ".java"
+dart = ".dart"
+
+#keywords for extensions
 extensions = {
     "text": text, "txt": text, ".txt": text,
     "markdown": markdown, "mark": markdown, ".md": markdown,
@@ -17,33 +35,44 @@ extensions = {
 }
 
 
-text = ".txt"
-markdown = ".md"
-python = ".py"
-c = ".c"
-cpp = ".cpp"
-matlab = ".m"
-shell = ".sh"
-javascript = ".js"
-typescript = ".ts"
-html = ".html"
-css = ".css"
+"""
+    nfe ==> note folder extension
+    - note ==> name of the file you want to open or create
+    - folder ==> name of the folder in which you want to create or open a file if exists otherwise create the folder and open or create the file
+    - extension ==> name of the extension in which you want to open the file(default is set to '.txt') also the extensions are according to my workflow
 
-java = ".java"
-dart = ".dart"
+"""
 
 def create():
     extension = str(sys.argv[3])
+    foldername = str(sys.argv[2])
+    filename = str(sys.argv[1])
+    os.chdir("./Notes")
+
+    #confirming the extension of the file
     try:
         extension = extensions[extension]
     except Exception:
-        print(extension)
+        print("EXT_ERROR: '{}' extension does not exist".format(extension))
+        print("FIX: opening file with default extension '.txt'")
         extension='.txt'
+    filename+=extension
 
-        
-    print(sys.argv)
-    filename = str(sys.argv[1]) + str(sys.argv[3])
-    open(filename, "a").close()
+    #confirming the directory of the file
+    if(os.path.isdir("./" + foldername)):
+        os.chdir("./" + foldername)
+    else:
+        print("DIR_ERROR: '{}' directory does not exist".format(foldername))
+        print("FIX: creating '{}' directory".format(foldername))
+        os.mkdir(foldername)
+        os.chdir("./" + foldername)
+
+    #confirming the file
+    if(not os.path.isfile("./" + filename)):
+        print("FILE_ERROR: '{}' file does not exist".format(filename))
+        print("FIX: creating '{}' file".format(filename))
+        open(filename, "a").close()
+    
     os.system("code " + filename)
 
 if __name__ == "__main__":
