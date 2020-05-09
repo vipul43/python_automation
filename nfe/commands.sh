557d419a598530592ca1ba51741a11b22f25290a
vipul43 <saifunny43@gmail.com>
2020-05-09 23:01:49 +0530

    - the navigating path is the path containing notes.py

--- nfe/commands.sh
+++ nfe/commands.sh
@@ -3,7 +3,7 @@
 function nfe() {
     PRESENT_DIR=$(pwd)
     cd
-    cd /Users/vipul/Documents/coding/python_automation/nfe/Notes
+    cd /Users/vipul/Documents/coding/python_automation/nfe
     python notes.py $1 $2 $3
     cd $PRESENT_DIR
 }
