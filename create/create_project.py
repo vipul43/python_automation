926231cc9db7ae772bc9488faf45a1937451ba4c
vipul43 <saifunny43@gmail.com>
2020-05-14 13:21:40 +0530

    - added checks for selenium errors - modified executable_path argument - executable_path argument must contain the path of geckodriver including the geckodriver file

--- create/create_project.py
+++ create/create_project.py
@@ -18,51 +18,55 @@ def folderAlreadyExists(title):
 
     
 def createRepoGithub(title):
-    #selenium part
-    options = Options()
-    options.headless = True
-    #running in headless mode
-    browser = webdriver.Firefox(options=options, executable_path=r'/Users/vipul')
-    #open browser and get github site
-    browser.get("https://github.com/login")
-    time.sleep(1)
+    try:
+        #selenium part
+        options = Options()
+        options.headless = True
+        #running in headless mode
+        browser = webdriver.Firefox(options=options, executable_path=r'/Users/vipul/geckodriver')
+        #open browser and get github site
+        browser.get("https://github.com/login")
+        time.sleep(1)
 
-    #enter username
-    username = browser.find_elements_by_xpath("//*[@id='login_field']")[0]
-    username.send_keys(user)
-    time.sleep(1)
+        #enter username
+        username = browser.find_elements_by_xpath("//*[@id='login_field']")[0]
+        username.send_keys(user)
+        time.sleep(1)
 
-    #enter pass word
-    password = browser.find_elements_by_xpath("//*[@id='password']")[0]
-    password.send_keys(secret)
-    time.sleep(1)
+        #enter pass word
+        password = browser.find_elements_by_xpath("//*[@id='password']")[0]
+        password.send_keys(secret)
+        time.sleep(1)
 
-    #sign in account
-    sign_in = browser.find_elements_by_xpath("/html/body/div[3]/main/div/form/div[4]/input[9]")[0]
-    sign_in.click()
-    time.sleep(4)
+        #sign in account
+        sign_in = browser.find_elements_by_xpath("/html/body/div[3]/main/div/form/div[4]/input[9]")[0]
+        sign_in.click()
+        time.sleep(4)
 
-    #get new repo creating page
-    browser.get("https://github.com/new")
-    time.sleep(1)
+        #get new repo creating page
+        browser.get("https://github.com/new")
+        time.sleep(1)
 
-    #enter new repo name
-    repo_name = browser.find_elements_by_xpath("//*[@id='repository_name']")[0]
-    repo_name.send_keys(title)
-    time.sleep(1)
+        #enter new repo name
+        repo_name = browser.find_elements_by_xpath("//*[@id='repository_name']")[0]
+        repo_name.send_keys(title)
+        time.sleep(1)
 
-    #initialize new repo with read me 
-    initialize_readme = browser.find_elements_by_xpath("//*[@id='repository_auto_init']")[0]
-    initialize_readme.click()
-    time.sleep(1)
+        #initialize new repo with read me 
+        initialize_readme = browser.find_elements_by_xpath("//*[@id='repository_auto_init']")[0]
+        initialize_readme.click()
+        time.sleep(1)
 
-    #create repo
-    create_repo = browser.find_elements_by_css_selector('button.first-in-line')[0]
-    create_repo.submit()
-    time.sleep(8)
+        #create repo
+        create_repo = browser.find_elements_by_css_selector('button.first-in-line')[0]
+        create_repo.submit()
+        time.sleep(8)
 
-    # quit browser
-    browser.quit()
+        # quit browser
+        browser.quit()
+        return True
+    except Exception:
+        return False
 
 
 def createProject():
@@ -78,16 +82,19 @@ def createProject():
         # if no proceeding to create new folder and git repo
         if(not folderAlreadyExists(title)):
             #create repo
-            createRepoGithub(title)
-            #create directory
-            clone_link = "https://github.com/vipul43/" + title + ".git"
-            os.system("git clone " + clone_link)
-            os.system("code " + title)
-            os.system("cd ./" + title)
-            #exit with success message
-            #checking for this success message in shell script
-            #VERY IMP PRINT STATEMENT
-            print("SUCCESS")
+            worked = createRepoGithub(title)
+            if(not worked):
+                print("SEL_ERR")
+            else:
+                #create directory
+                clone_link = "https://github.com/vipul43/" + title + ".git"
+                os.system("git clone " + clone_link)
+                os.system("code " + title)
+                os.system("cd ./" + title)
+                #exit with success message
+                #checking for this success message in shell script
+                #VERY IMP PRINT STATEMENT
+                print("SUCCESS")
         else:
             # else raise error
             print("DIR_ERR")
