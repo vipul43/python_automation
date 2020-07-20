253a5f0510c0477af119a86f9f221e0ac0c35b6d
vipul43 <saifunny43@gmail.com>
2020-07-20 15:32:03 +0530

    cp file creation and template copying automation

--- cpfile/commands.sh
+++ cpfile/commands.sh
@@ -3,14 +3,7 @@ function cpfile() {
     PRESENT_DIR=$(pwd)
     cd
     cd /Users/vipul/Documents/coding/python_automation/cpfile
-    MSG=$(python cp_file_creation.py $1 $PRESENT_DIR)
+    python cp_file_creation.py $1 $PRESENT_DIR
     cd
     cd $PRESENT_DIR
-    if [ $MSG = "SUCCESS" ]
-    then
-        echo "EXIT: 0"
-        echo "Successfully created cp file"
-        echo "Successfully copied template file"
-        echo "FINISHED"
-    fi
 }
--- cpfile/cp_file_creation.py
+++ cpfile/cp_file_creation.py
@@ -3,9 +3,14 @@ import os
 
 filename = sys.argv[1]
 filedir = sys.argv[2]
+
 os.chdir(filedir)
 os.system('touch ' + filename)
+
 os.chdir('/Users/vipul/Documents/coding/cp')
-cp_file_path = './cp_cpp_template.cpp'
+file, extension = filename.split('.')
+if(extension=="py"):
+    cp_file_path = './cp_python_template.py'
+elif(extension=="cpp"):
+    cp_file_path = './cp_cpp_template.cpp'
 os.system('cp ' + cp_file_path + ' ../../../../..' + filedir + '/' + filename)
-print("SUCCESS")
